cb66ffc76119c82da637be9467058c5ee0a242bd
Mike-Heneghan <m.heneghan@live.co.uk>
2018-06-09 22:12:18 +0100

    refactored add test code.

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -11,33 +11,41 @@ describe('calculator', function () {
     assert.equal(true, true)
   });
 
-  it('should be able to add two numbers as selected by user', function() {
-    calculator.numberClick(5.56);
-    calculator.operatorClick('+');
-    calculator.numberClick(6.29);
-    calculator.operatorClick('=');
-    const actual = calculator.runningTotal;
-    assert.strictEqual(actual, 11.85);
-  });
+// These tests aren't specific enough.
+  // it('should be able to add two numbers as selected by user', function() {
+  //   calculator.numberClick(5.56);
+  //   calculator.operatorClick('+');
+  //   calculator.numberClick(6.29);
+  //   calculator.operatorClick('=');
+  //   const actual = calculator.runningTotal;
+  //   assert.strictEqual(actual, 11.85);
+  // });
+  //
+  // it('should be able to add multiple numbers as selected by user', function(){
+  //   calculator.numberClick(0.937);
+  //   calculator.operatorClick('+');
+  //   calculator.numberClick(89.03);
+  //   calculator.operatorClick('+');
+  //   calculator.numberClick(45.7);
+  //   calculator.operatorClick('=');
+  //   const actual = calculator.runningTotal;
+  //   assert.strictEqual(actual, 135.667);
+  // });
+  //
+  // it('should be able to subtract two numbers', function() {
+  //   calculator.numberClick(33.987);
+  //   calculator.operatorClick('-');
+  //   calculator.numberClick(16.78);
+  //   calculator.operatorClick('=');
+  //   const actual = calculator.runningTotal;
+  //   assert.strictEqual(actual, 17.207);
+  // });
 
-  it('should be able to add multiple numbers as selected by user', function(){
-    calculator.numberClick(0.937);
-    calculator.operatorClick('+');
-    calculator.numberClick(89.03);
-    calculator.operatorClick('+');
-    calculator.numberClick(45.7);
-    calculator.operatorClick('=');
+  it('should be able to add two numbers', function() {
+    calculator.previousTotal = 10.678;
+    calculator.add(22.56);
     const actual = calculator.runningTotal;
-    assert.strictEqual(actual, 135.667);
-  });
-
-  it('should be able to subtract two numbers', function() {
-    calculator.numberClick(33.987);
-    calculator.operatorClick('-');
-    calculator.numberClick(16.78);
-    calculator.operatorClick('=');
-    const actual = calculator.runningTotal;
-    assert.strictEqual(actual, 17.207);
+    assert.strictEqual(actual, 33.238);
   });
 
 });
