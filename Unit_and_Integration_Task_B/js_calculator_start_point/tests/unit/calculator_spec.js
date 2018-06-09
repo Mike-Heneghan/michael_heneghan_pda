667247d77096af44f7fa2fe438e3186c0f56268e
Mike-Heneghan <m.heneghan@live.co.uk>
2018-06-09 21:30:58 +0100

    subtraction testing completed.

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -11,15 +11,33 @@ describe('calculator', function () {
     assert.equal(true, true)
   });
 
-  it('should be able to add two numbers', function() {
+  it('should be able to add two numbers as selected by user', function() {
     calculator.numberClick(5.56);
     calculator.operatorClick('+');
     calculator.numberClick(6.29);
     calculator.operatorClick('=');
     const actual = calculator.runningTotal;
-    assert.equal(actual, 11.85);
+    assert.strictEqual(actual, 11.85);
   });
 
-  // it('should be able to subtract two numbers', )
+  it('should be able to add multiple numbers as selected by user', function(){
+    calculator.numberClick(0.937);
+    calculator.operatorClick('+');
+    calculator.numberClick(89.03);
+    calculator.operatorClick('+');
+    calculator.numberClick(45.7);
+    calculator.operatorClick('=');
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, 135.667);
+  });
+
+  it('should be able to subtract two numbers', function() {
+    calculator.numberClick(33.987);
+    calculator.operatorClick('-');
+    calculator.numberClick(16.78);
+    calculator.operatorClick('=');
+    const actual = calculator.runningTotal;
+    assert.strictEqual(actual, 17.207);
+  });
 
 });
