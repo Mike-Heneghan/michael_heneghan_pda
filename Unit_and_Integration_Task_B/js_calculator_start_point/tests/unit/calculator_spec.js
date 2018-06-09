3aab7ec712812d52e22bfd09ac449c49c0f3e803
Mike-Heneghan <m.heneghan@live.co.uk>
2018-06-09 23:19:15 +0100

    added comment to remember about changing the decimals to whole numbers.

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -11,35 +11,7 @@ describe('calculator', function () {
     assert.equal(true, true)
   });
 
-// These tests aren't specific enough.
-  // it('should be able to add two numbers as selected by user', function() {
-  //   calculator.numberClick(5.56);
-  //   calculator.operatorClick('+');
-  //   calculator.numberClick(6.29);
-  //   calculator.operatorClick('=');
-  //   const actual = calculator.runningTotal;
-  //   assert.strictEqual(actual, 11.85);
-  // });
-  //
-  // it('should be able to add multiple numbers as selected by user', function(){
-  //   calculator.numberClick(0.937);
-  //   calculator.operatorClick('+');
-  //   calculator.numberClick(89.03);
-  //   calculator.operatorClick('+');
-  //   calculator.numberClick(45.7);
-  //   calculator.operatorClick('=');
-  //   const actual = calculator.runningTotal;
-  //   assert.strictEqual(actual, 135.667);
-  // });
-  //
-  // it('should be able to subtract two numbers', function() {
-  //   calculator.numberClick(33.987);
-  //   calculator.operatorClick('-');
-  //   calculator.numberClick(16.78);
-  //   calculator.operatorClick('=');
-  //   const actual = calculator.runningTotal;
-  //   assert.strictEqual(actual, 17.207);
-  // });
+  // Maybe shouldn't have used decimals as they aren't actually possible to input with the calculator?
 
   it('should be able to add two numbers', function() {
     calculator.previousTotal = 10.678;
@@ -102,3 +74,33 @@ describe('calculator', function () {
   });
 
 });
+
+// These tests aren't specific enough.
+  // it('should be able to add two numbers as selected by user', function() {
+  //   calculator.numberClick(5.56);
+  //   calculator.operatorClick('+');
+  //   calculator.numberClick(6.29);
+  //   calculator.operatorClick('=');
+  //   const actual = calculator.runningTotal;
+  //   assert.strictEqual(actual, 11.85);
+  // });
+  //
+  // it('should be able to add multiple numbers as selected by user', function(){
+  //   calculator.numberClick(0.937);
+  //   calculator.operatorClick('+');
+  //   calculator.numberClick(89.03);
+  //   calculator.operatorClick('+');
+  //   calculator.numberClick(45.7);
+  //   calculator.operatorClick('=');
+  //   const actual = calculator.runningTotal;
+  //   assert.strictEqual(actual, 135.667);
+  // });
+  //
+  // it('should be able to subtract two numbers', function() {
+  //   calculator.numberClick(33.987);
+  //   calculator.operatorClick('-');
+  //   calculator.numberClick(16.78);
+  //   calculator.operatorClick('=');
+  //   const actual = calculator.runningTotal;
+  //   assert.strictEqual(actual, 17.207);
+  // });
