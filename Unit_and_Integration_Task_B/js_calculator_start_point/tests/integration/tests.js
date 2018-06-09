63020b7353ccc2713f5dc2c3037f9804a8bd0aaa
Mike-Heneghan <m.heneghan@live.co.uk>
2018-06-09 22:49:23 +0100

    Possible to  chain together multiple operations.

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -23,6 +23,30 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('72')
   });
 
+  it('should be able to use + arithmetical operations to update the display', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number7')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('9')
+  });
+
+  it('should be able to string multiple operations', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number7')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  })
+
 });
 
 // You should write tests to ensure:
